31191739934f44f49aeebaf450d41e1dacc2e9f9
unknown <you@example.com>
2025-04-17 15:57:58 -0300

    Refatorado o projeto: modificado a lógica da função editarOpjeto

--- projeto_imagens/js/index.js
+++ projeto_imagens/js/index.js
@@ -16,8 +16,11 @@ btns.forEach(btn =>{
 
 
 function pressButton(e){
+    // origem do evento
     let alvo = e.target
+
     let value = parseInt(alvo.value)
+    
     // seleção
     let h2 = document.querySelector('h2')
     let img = document.querySelector('img')
@@ -33,43 +36,43 @@ function pressButton(e){
 
 function editarObjeto(value){
     var obj = {
-        h2: "",
-        img: "",
-        descricao: ""
+        h2: cidades[value-1].titulo,
+        img: cidades[value-1].img,
+        descricao: cidades[value-1].descricao
     }
 
-    switch (value) {
-        case 1:
-            obj.h2 = cidades[0].titulo
-            obj.img = cidades[0].img
-            obj.descricao = cidades[0].descricao
-            break;
-        case 2:
-            obj.h2 = cidades[1].titulo
-            obj.img = cidades[1].img
-            obj.descricao = cidades[1].descricao
-            break;
-        case 3:
-            obj.h2 = cidades[2].titulo
-            obj.img = cidades[2].img
-            obj.descricao = cidades[2].descricao
-            break;
-        case 4:
-            obj.h2 = cidades[3].titulo
-            obj.img = cidades[3].img
-            obj.descricao = cidades[3].descricao
-            break;
-        case 5:
-            obj.h2 = cidades[4].titulo
-            obj.img = cidades[4].img
-            obj.descricao = cidades[4].descricao
-            break;
-        case 6:
-            obj.h2 = cidades[5].titulo
-            obj.img = cidades[5].img
-            obj.descricao = cidades[5].descricao
-            break;
-    }
+    // switch (value) {
+    //     case 1:
+    //         obj.h2 = cidades[0].titulo
+    //         obj.img = cidades[0].img
+    //         obj.descricao = cidades[0].descricao
+    //         break;
+    //     case 2:
+    //         obj.h2 = cidades[1].titulo
+    //         obj.img = cidades[1].img
+    //         obj.descricao = cidades[1].descricao
+    //         break;
+    //     case 3:
+    //         obj.h2 = cidades[2].titulo
+    //         obj.img = cidades[2].img
+    //         obj.descricao = cidades[2].descricao
+    //         break;
+    //     case 4:
+    //         obj.h2 = cidades[3].titulo
+    //         obj.img = cidades[3].img
+    //         obj.descricao = cidades[3].descricao
+    //         break;
+    //     case 5:
+    //         obj.h2 = cidades[4].titulo
+    //         obj.img = cidades[4].img
+    //         obj.descricao = cidades[4].descricao
+    //         break;
+    //     case 6:
+    //         obj.h2 = cidades[5].titulo
+    //         obj.img = cidades[5].img
+    //         obj.descricao = cidades[5].descricao
+    //         break;
+    // }
 
     return obj
 }
